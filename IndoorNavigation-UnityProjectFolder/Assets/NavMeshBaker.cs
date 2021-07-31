d66ed9f015e96ce22488fd632adef09ca11e71cb
advoworks <advoworks@gmail.com>
2021-07-31 14:54:27 +0800

    Troubleshooting Navmesh losing alignment with AreaTargets
    Installed NavMeshComponents
    
    NavMesh prefabs in AreaTargets left there but in NavMesh layer which is culled by camera. The hope is that the MultiArea script auto orients them to the AreaTarget along with the other augmentations...
    
    This way the NavMesh runtime baking should take care of it
    
    Still not working wel

---
+++ IndoorNavigation-UnityProjectFolder/Assets/NavMeshBaker.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBaker : MonoBehaviour
+{
+
+    public bool bakeUponStart = true;
+    public bool autoRefresh = true;
+
+    public float recalculationTime = 2f;
+    public NavMeshSurface[] surfaces;
+    private float time = 0f;
+
+    private void Start()
+    {
+        if (!bakeUponStart) return;
+        UpdateNavMesh();
+    }
+
+    private void Update()
+    {
+
+        if (!autoRefresh) return;
+
+        time += Time.deltaTime;
+
+        if (time >= recalculationTime)
+        {
+            time = 0f;
+            Debug.Log(Time.time + "Updating NavMesh");
+            UpdateNavMesh();
+        }
+    }
+
+    //private void BakeNavMesh()
+    //{
+    //    for (int i = 0; i < surfaces.Length; i++)
+    //    {
+    //        Debug.Log("NavMeshBaker: BuildNavMesh for " + surfaces[i].name);
+    //        surfaces[i].BuildNavMesh();            
+    //    }
+    //}
+
+    public void UpdateNavMesh()
+    {
+        for (int i = 0; i < surfaces.Length; i++)
+        {
+            if (!surfaces[i].navMeshData)
+            {
+                Debug.Log("<color=red>NavMeshBaker: BuildNavMesh for " + surfaces[i].name + "</color>");
+                surfaces[i].BuildNavMesh();                
+            }
+            else
+            {
+                Debug.Log("<color=red>NavMeshBaker: UpdateNavMesh for " + surfaces[i].name + "</color>");
+                surfaces[i].UpdateNavMesh(surfaces[i].navMeshData);
+            }
+        }
+
+        //for (int i = 0; i < surfaces.Length; i++)
+        //{
+        //    surfaces[i].UpdateNavMesh(surfaces[i].navMeshData);
+
+        //}
+    }
+}
